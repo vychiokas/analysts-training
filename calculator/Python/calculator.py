bc8ff800fee409aef8fefff2ca510a83b4062702
Vytautas Sluckas <vytautas.sluckas@gjensidige.no>
2021-03-15 11:52:48 +0200

    implemeneting python calculator

--- calculator/Python/calculator.py
+++ calculator/Python/calculator.py
@@ -3,16 +3,16 @@
 
 def add(arg1, arg2):
   #   Your code goes here
-  return
+  return(arg1 + arg2)
 
 def subtract(arg1, arg2):
   #   Your code goes here
-  return
+  return(arg1 - arg2)
 
 def multiply(arg1, arg2):
   #   Your code goes here
-  return
+  return(arg1 * arg2)
 
 def divide(arg1, arg2):
   #   Your code goes here
-  return
+  return(arg1 / arg2)
